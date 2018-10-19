7fbaded9edc720a3e605fd81be5830959964dc80
Richard <ricgomen@gmail.com>
2018-10-20 00:16:02 +0200

    Añadidos logs

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -1,5 +1,7 @@
 package com.boot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +12,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App 
 {
+
     public static void main( String[] args )
     {
+    	Logger logger = LoggerFactory.getLogger(App.class);
+		logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+        
     	SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/com/boot/controller/HomeController.java
+++ src/main/java/com/boot/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.boot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 	
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 	@RequestMapping("/")
 	@ResponseBody
 	public String home() {
+		logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+        
 		return "Das Boot, reporting for duty!";
 	}
 
